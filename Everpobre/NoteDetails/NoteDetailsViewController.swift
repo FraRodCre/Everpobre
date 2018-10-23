f1122c7afc2ad4b9490335a2ef4984e97d7af7cf
Fco_Javier_Rodriguez <fco_javier@Mac-de-Fco-Javier-Rodriguez.local>
2018-10-23 18:21:36 +0200

    Add NotesDetails

---
+++ Everpobre/NoteDetails/NoteDetailsViewController.swift
@@ -0,0 +1,49 @@
+//
+//  NoteDetailsViewController.swift
+//  Everpobre
+//
+//  Created by Fco_Javier_Rodriguez on 23/10/18.
+//  Copyright © 2018 Fco_Javier_Rodriguez. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailsViewController: UIViewController {
+    
+    // MARK: IBOulets
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var tagsLabel: UILabel!
+    @IBOutlet weak var creationDateLabel: UILabel!
+    @IBOutlet weak var lastSeenDateLabel: UILabel!
+    @IBOutlet weak var descriptionTextView: UITextView!
+    
+    // MARK: Properties
+    let note: Note
+    
+    // MARK: Initializers (Markers)
+    init(note: Note) {
+        self.note = note
+        super.init(nibName: "NoteDetailsViewController", bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+    }
+    
+    // Change information of a note
+    private func configure() {
+        title = "Detalle"
+        titleLabel.text = note.text
+        tagsLabel.text = note.tags?.joined(separator: ",")
+        creationDateLabel.text = note.creationDate.dateToString()
+        lastSeenDateLabel.text = note.lastSeenDate?.dateToString() ?? ""
+        descriptionTextView.text = note.text ?? "Introducir texto..."
+    }
+}
