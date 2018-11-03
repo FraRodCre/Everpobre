da3fb969317f6c577589d43b7b41a972d1d42f64
Fco_Javier_Rodriguez <fco_javier@Mac-de-Fco-Javier-Rodriguez.local>
2018-11-03 20:14:11 +0100

    Add functionality add image in note datils

--- Everpobre/NoteDetails/NoteDetailsViewController.swift
+++ Everpobre/NoteDetails/NoteDetailsViewController.swift
@@ -42,7 +42,8 @@ class NoteDetailsViewController: UIViewController {
     /* Before coreData
      init(note: Note //NoteOld) {
         self.note = note
-        super.init(nibName: "NoteDetailsViewController", bundle: nil)
+     
+     super.init(nibName: "NoteDetailsViewController", bundle: nil)
     }*/
     
     init (kind: Kind, managedContext: NSManagedObjectContext) {
@@ -57,71 +58,113 @@ class NoteDetailsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configure(with: kind)
+        configure()
     }
     
     // Change information of a note
-    private func configure(with kind: Kind) {
-        switch kind {
-        case .new:
-            
+    private func configure() {
             let saveButtonItem = UIBarButtonItem(barButtonSystemItem: .save, target: self, action: #selector(saveNote))
             self.navigationItem.rightBarButtonItem = saveButtonItem
             
             let cancelButtomItem = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancel))
             navigationItem.leftBarButtonItem = cancelButtomItem
             configureValues()
-        case .existing:
-            configureValues()
-        }
-        
     }
     
     @objc private func saveNote() {
+        
+        func addProperties(to note: Note) -> Note {
+            note.title = titleTextField.text
+            note.text = descriptionTextView.text
+            
+            let imageData: NSData?
+            if let image = imageView.image,
+                let data = image.pngData() {
+                    imageData = NSData(data: data)
+            } else { imageData = nil }
+            note.image = imageData
+            
+            return note
+        }
+        
         switch kind {
         case .existing(let note):
             
-            note.title = titleTextField.text
+            /*note.title = titleTextField.text
             note.text = descriptionTextView.text
-            note.lastSeenDate = NSDate()
-            
-            // save context
-            do {
-               try managedContext.save()
-                delegate?.didSaveNote()
-            }catch let error as NSError {
-                print("Error: \(error.localizedDescription)")
-            }
-            
-            navigationController?.popViewController(animated: true)
+             note.lastSeenDate = NSDate()*/
+            let modifiedNote = addProperties(to: note)
+            modifiedNote.lastSeenDate = NSDate()
             
         case .new(let notebook):
             let note = Note(context: managedContext)
-            note.title = titleTextField.text
+            /*note.title = titleTextField.text
             note.text = descriptionTextView.text
             note.creationDate = NSDate()
-            note.notebook = notebook
+             note.notebook = notebook */
+            let modifiedNote = addProperties(to: note)
+            modifiedNote.notebook = notebook
             
             if let notes = notebook.notes?.mutableCopy() as? NSMutableOrderedSet {
                 notes.add(note)
                 notebook.notes = notes
             }
-            
-            // save context
-            do {
-                try managedContext.save()
-                delegate?.didSaveNote()
-            }catch let error as NSError {
-                print("Error: \(error.localizedDescription)")
-            }
-            
+        }
+        // save context
+        do {
+            try managedContext.save()
+            delegate?.didSaveNote()
+        }catch let error as NSError {
+            print("Error: \(error.localizedDescription)")
+        }
+        
+        switch kind {
+        case .existing:
+            navigationController?.popViewController(animated: true)
+        case .new:
             dismiss(animated: true, completion: nil)
-            
         }
     }
     
     @objc private func cancel() {
         dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func pickImage(_ sender: UIButton) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            showPhotoMenu()
+        } else {
+            choosePhotoFromLibrary()
+        }
+    }
+    
+    private func showPhotoMenu() {
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .alert)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        let takePhotoAction = UIAlertAction(title: "Take Photo", style: .default, handler: {_ in self.takePhotoWithCamara()})
+        let chooseFromLibrary = UIAlertAction(title: "Choose from Library", style: .default, handler: {_ in self.choosePhotoFromLibrary()})
+        
+        alertController.addAction(cancelAction)
+        alertController.addAction(takePhotoAction)
+        alertController.addAction(chooseFromLibrary)
+        
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    private func choosePhotoFromLibrary() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .photoLibrary
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    private func takePhotoWithCamara() {
+        let imagePicker = UIImagePickerController()
+        imagePicker.sourceType = .camera
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = true
+        present(imagePicker, animated: true, completion: nil)
     }
     
     private func configureValues() {
@@ -131,9 +174,16 @@ class NoteDetailsViewController: UIViewController {
         /*tagsLabel.text = note.tags?.joined(separator: ",")
          creationDateLabel.text = note.creationDate.dateToString()
          lastSeenDateLabel.text = note.lastSeenDate?.dateToString() ?? ""*/
-        creationDateLabel.text = (kind.note?.creationDate as Date?)?.dateToString() ?? ""
-        lastSeenDateLabel.text = (kind.note?.lastSeenDate as Date?)?.dateToString() ?? ""
+        creationDateLabel.text = (kind.note?.creationDate as Date?)?.dateToString() ?? "No disponible"
+        lastSeenDateLabel.text = (kind.note?.lastSeenDate as Date?)?.dateToString() ?? "No disponible"
         descriptionTextView.text = kind.note?.text ?? "Introducir texto..."
+        
+        guard let data = kind.note?.image as Data? else {
+            imageView.image = #imageLiteral(resourceName: "120x180")
+            return
+        }
+        
+        imageView.image = UIImage(data: data)
     }
 }
 
@@ -150,5 +200,43 @@ private extension NoteDetailsViewController.Kind {
         case .new:
             return "Nota nueva"
         }
+    }
+}
+
+extension NoteDetailsViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+            return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+        }
+        
+        func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+            return input.rawValue
+        }
+        
+        let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+        
+        let rawImage = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.editedImage)] as? UIImage
+        
+        let imageSize = CGSize(width: self.imageView.bounds.width * UIScreen.main.scale, height: self.imageView.bounds.height * UIScreen.main.scale)
+        
+        DispatchQueue.global(qos: .default).async {
+            let image = rawImage?.resizedImageWithContentMode(.scaleAspectFill, bounds: imageSize, interpolationQuality: .high)
+            
+            DispatchQueue.main.async {
+                if let image = image {
+                    self.imageView.contentMode = .scaleAspectFill
+                    self.imageView.clipsToBounds = true
+                    self.imageView.image = image
+                }
+            }
+        }
+        
+        dismiss(animated: true, completion: nil)
     }
 }
